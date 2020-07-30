88e2716ab9b50614dbb13d94964c142550cfa94c
Haiyang Hong <hong961127@gmail.com>
2020-07-30 11:46:37 -0700

    Setup User schema

---
+++ model/User.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    required: [true, "Please provide an email."],
+    unique: [true, "This email is already taken."],
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "Please provide a valid email address.",
+    },
+  },
+  amazonOrderId: {
+    type: String,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    trim: true,
+    required: [true, "Please provide a first name."],
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: [true, "Please provide a last name."],
+  },
+  address: {
+    type: String,
+    required: [true, "Please provide an address."],
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isMobilePhone(v),
+      message: "Please provide a valid phone number.",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password."],
+    minlength: [8, "A password must be at least 8 characters long"],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please confirm your password."],
+    validate: {
+      validator: function (v) {
+        return v === this.password;
+      },
+      message: "Passwords do not match.",
+    },
+  },
+  passwordResetToken: {
+    type: String,
+  },
+  resetTokenExpires: {
+    type: Date,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  level: {
+    type: String,
+    enum: ["Pioneer", "Cesuner"],
+    default: "Cesuner",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
