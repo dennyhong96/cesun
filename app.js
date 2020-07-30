9a46b1d3d135b5602f40262d63de6eecfc12e25f
Haiyang Hong <hong961127@gmail.com>
2020-07-30 14:37:32 -0700

    Add utils, start on products endpoints

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
 
 const connectDB = require("./config/db");
+const productRouter = require("./router/products");
 
-const app = express();
 connectDB();
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Mount Routers
+app.use("/api/v1/products", productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server up on port ${port}...`));
---
+++ controller/products.js
@@ -0,0 +1,6 @@
+const asyncHandler = require("../utils/asyncHandler");
+
+exports.createProduct = asyncHandler(async (req, res, next) => {
+  try {
+  } catch (error) {}
+});
